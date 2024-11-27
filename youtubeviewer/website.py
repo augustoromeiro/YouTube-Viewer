b80be44717500ea3aeed70b204b2b5e657b83a64
augustoromeiro <augusto.c.romeiro@gmail.com>
2024-11-27 15:56:03 -0300

    update

--- youtubeviewer/website.py
+++ youtubeviewer/website.py
@@ -158,4 +158,4 @@ def start_server(host, port, debug=False):
 
 
 if __name__ == '__main__':
-    start_server(host='0.0.0.0', port=5001, debug=True)
+    start_server(host='0.0.0.0', port=5000, debug=True)
